f35d3fb3cf644e1de243fdae1470a651ac5ea43a
chandu_naik <109847892+chandu3333@users.noreply.github.com>
2023-05-09 15:00:04 +0530

    app.py

--- app.py
+++ app.py
@@ -35,14 +35,15 @@ touch_screen  = st.text_input("enter touch_screen",placeholder="enter value")
 wifi = st.text_input("enter wifi",placeholder="enter value")
 
 btn = st.button("Predict")
-
+col_names = ['battery_power','clock_speed','fc','int_memory','m_dep','mobile_wt','n_cores','pc','px_height','px_width','ram','sc_h','sc_w','talk_time','three_g','touch_screen','wifi','blue_NO','blue_yes','dual_sim_NO','dual_sim_yes','four_g_NO','four_g_yes']
 if btn == True:
     if battery_power and blue and clock_speed and dual_sim and fc and four_g and int_memory and clock_speed and m_dep and mobile_wt and n_cores and pc and px_height and px_width and sc_h and sc_w and talk_time and three_g and touch_screen and wifi:
-        q_p = np.array([int(battery_power), blue, float(clock_speed), dual_sim, int(fc), four_g, int(int_memory), float(m_dep), float(mobile_wt), int(n_cores), int(pc), float(px_height), float(px_width), float(ram), int(sc_h), int(sc_w), int(talk_time), int(three_g),int(touch_screen), int(wifi)])
-        q_p2 = ohen.transform(q_p)
-        pred = knm.predict(q_p2)
+        q_p = pd.DataFrame([[int(battery_power), float(clock_speed), int(fc), int(int_memory), float(m_dep), float(mobile_wt), int(n_cores), int(pc), float(px_height), float(px_width), float(ram), int(sc_h), int(sc_w), int(talk_time), int(three_g),int(touch_screen), int(wifi)]])
+        q_p1 = pd.DataFrame([[blue,dual_sim,four_g]])
+        q_p2 = ohen.transform(q_p1).toarray()
+        q_p3 = pd.concat([q_p,pd.DataFrame(q_p2)],axis=1)
+        q_p3.columns=col_names
+        pred = knm.predict(q_p3)
         st.success(pred)
-    else:
+else:
         st.error("enter the value properly.")
-
-#joblib.dump(kmodel,open("final.joblib","wb"))
